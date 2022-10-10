330f0f38ac94376b23aac2de657cf9884aa40114
domia2814 <106426622+domia2814@users.noreply.github.com>
2022-10-10 14:51:05 +0200

    aplikacja todo

---
+++ main.js
@@ -0,0 +1,57 @@
+const form = document.querySelector('.new-tasks-form')
+const input = document.querySelector('.new-task-add')
+const listEl = document.querySelector('.tasks')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const task = input.value
+    if(!task) {
+        alert("Proszę wypełnić pole ")
+    } else {
+        console.log("Success")
+    }
+
+    const taskEl = document.createElement("div")
+    taskEl.classList.add("task-list-content")
+
+    listEl.appendChild(taskEl)
+
+    const taskInput = document.createElement("input")
+    taskInput.classList.add("task-list-input")
+    taskInput.type = "text"
+    taskInput.value = task
+    taskInput.setAttribute("readonly", "readonly")
+
+    taskEl.appendChild(taskInput)
+
+    const taskButton = document.createElement("button")
+    taskButton.classList.add("task-list-edit")
+    taskButton.innerHTML = "Edytuj"
+      
+    taskEl.appendChild(taskButton)
+
+    const taskButton2 = document.createElement("button")
+    taskButton2.classList.add("task-list-delete")
+    taskButton2.innerHTML = "Usuń"
+
+    taskEl.appendChild(taskButton2)
+
+    taskButton.addEventListener('click', () => {
+       if(taskButton.innerText == "Edytuj") {
+        taskInput.removeAttribute("readonly")
+        taskInput.focus()
+        taskButton.innerText = "Save"
+       } else {
+        taskInput.setAttribute("readonly", "readonly")
+        taskButton.innerText = "Edytuj"
+       }
+
+    })
+
+    taskButton2.addEventListener('click', () => {
+        listEl.removeChild(taskEl)
+     })
+
+})
+
+
