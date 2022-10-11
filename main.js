07398f950c118eb0e88b288d6db3bfab33417406
kkrawiec <kkrawiec@sii.pl>
2022-10-11 10:14:34 +0200

    Podzial na funkcje, zmiana nazewnictwa

--- main.js
+++ main.js
@@ -1,57 +1,68 @@
 const form = document.querySelector('.new-tasks-form')
 const input = document.querySelector('.new-task-add')
-const listEl = document.querySelector('.tasks')
+const tasksList = document.querySelector('.tasks')
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const task = input.value
-    if(!task) {
+    const taskName = input.value
+    if(!taskName) {
         alert("Proszę wypełnić pole ")
     } else {
         console.log("Success")
+        addTaskToList()
     }
-
-    const taskEl = document.createElement("div")
-    taskEl.classList.add("task-list-content")
-
-    listEl.appendChild(taskEl)
-
-    const taskInput = document.createElement("input")
-    taskInput.classList.add("task-list-input")
-    taskInput.type = "text"
-    taskInput.value = task
-    taskInput.setAttribute("readonly", "readonly")
-
-    taskEl.appendChild(taskInput)
-
-    const taskButton = document.createElement("button")
-    taskButton.classList.add("task-list-edit")
-    taskButton.innerHTML = "Edytuj"
-      
-    taskEl.appendChild(taskButton)
-
-    const taskButton2 = document.createElement("button")
-    taskButton2.classList.add("task-list-delete")
-    taskButton2.innerHTML = "Usuń"
-
-    taskEl.appendChild(taskButton2)
-
-    taskButton.addEventListener('click', () => {
-       if(taskButton.innerText == "Edytuj") {
-        taskInput.removeAttribute("readonly")
-        taskInput.focus()
-        taskButton.innerText = "Save"
-       } else {
-        taskInput.setAttribute("readonly", "readonly")
-        taskButton.innerText = "Edytuj"
-       }
-
-    })
-
-    taskButton2.addEventListener('click', () => {
-        listEl.removeChild(taskEl)
-     })
-
 })
 
+function addTaskToList() {
+    const taskContent = document.createElement("div")
+    taskContent.classList.add("task-list-content")
 
+    taskContent.appendChild(createTaskInput())
+
+    taskContent.appendChild(createTaskButtonEdit())
+
+    taskContent.appendChild(createTaskButtonRemove())
+
+    tasksList.appendChild(taskContent)
+}
+
+function createTaskInput() {
+    let input = document.createElement("input")
+    input.classList.add("task-list-input")
+    input.type = "text"
+    input.value = task
+    input.setAttribute("readonly", "readonly")
+    return input;
+}
+
+function createTaskButtonEdit() {
+    let button = document.createElement("button")
+    button.classList.add("task-list-edit")
+    button.innerHTML = "Edytuj"
+
+    button.addEventListener('click', () => {
+        if(taskButton.innerText == "Edytuj") {
+         taskInput.removeAttribute("readonly")
+         taskInput.focus()
+         taskButton.innerText = "Save"
+        } else {
+         taskInput.setAttribute("readonly", "readonly")
+         taskButton.innerText = "Edytuj"
+        }
+ 
+     })
+
+     return button
+}
+
+function createTaskButtonRemove() {
+    let button = document.createElement("button")
+    button.classList.add("task-list-delete")
+    button.innerHTML = "Usuń"
+
+    button.addEventListener('click', () => {
+        tasksList.removeChild(taskContent)
+    })
+
+    return button
+}
